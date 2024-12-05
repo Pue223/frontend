aea395dfcb17debbb10903f95f4f3145ab482ee7
Raul Alberto Mendizabal Garcia <sgrec30@MPL.COM>
2024-12-05 15:05:50 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  Card} from '@tremor/react';
+import {  Meta} from "./Meta.jsx";
+export const App = () => {
+  return (
+    <div>
+      <Card>
+        <Meta/>
+      </Card>
+    </div>
+  )
+}
---
+++ src/Meta.jsx
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    BarChart,
+    TableHead,
+    TableHeaderCell,
+    TableRow,
+    Card
+} from '@tremor/react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
+import axios from "axios";
+
+const dataFormatter = (number) =>
+    new Intl.NumberFormat("us").format(number).toString();
+
+export const Meta = () => {
+    const [data, setData] = useState([]);
+    const [dataId1, setDataId1] = useState([]);
+    const [dataId2, setDataId2] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:4001/api/totalmeta')
+            .then((response) => {
+                if (response.data.ok) {
+                    const fetchedData = response.data.meta;
+                    setData(fetchedData); // Set the data in state
+
+                    // Filtrar los datos según el id y transformarlos
+                    const dataId1 = fetchedData.filter(item => item.id === 1);
+                    const dataId2 = fetchedData.filter(item => item.id === 2);
+                    // Transformar los datos para cambiar las claves a nombres amigables
+                    const transformedData1 = dataId1.map((item) => ({
+                        id: item.id,
+                        "Ingresos Corrientes": item.totalImporte,
+                        "Umbral mínimo": item.um,
+                        "Avance": item.avance,
+                        "Diferencia": item.dif,
+                    }));
+
+                    const transformedData2 = dataId2.map((item) => ({
+                        id: item.id,
+                        "Ingresos Corrientes": item.totalImporte,
+                        "Umbral máximo": item.um,
+                        "Avance": item.avance,
+                        "Diferencia": item.dif,
+                    }));
+
+                    setDataId1(transformedData1);
+                    setDataId2(transformedData2);
+                }
+            })
+            .catch((error) => {
+                console.error("There was an error fetching the data:", error);
+            });
+    }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+    const formatCurrency = (value) => {
+        return new Intl.NumberFormat('es-PE', {
+            style: 'currency',
+            currency: 'PEN',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(value);
+    };
+
+    return (
+        <>
+            <Tabs defaultValue="tab1">
+                <TabsList className="space-y-6 flex flex-col">
+                    {/* Tab para el id 1 */}
+                    <TabsTrigger value="tab1">
+
+                        {dataId1.length > 0 && (
+                            <Card className="mx-auto max-w-2xl mb-6">
+                                <p>
+                                    Cuadro 1:
+                                </p>
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableHeaderCell>INGRESOS CORRIENTES</TableHeaderCell>
+                                            <TableHeaderCell>UMBRAL MINIMO</TableHeaderCell>
+                                            <TableHeaderCell>AVANCE</TableHeaderCell>
+                                            <TableHeaderCell>DIFERENCIA</TableHeaderCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {dataId1.map((item) => (
+                                            <TableRow key={item.id}>
+                                                <TableCell className="text-center">{formatCurrency(item["Ingresos Corrientes"])}</TableCell>
+                                                <TableCell className="text-center">{formatCurrency(item["Umbral mínimo"])}</TableCell>
+                                                <TableCell className="text-center">{item["Avance"].toFixed(2)}%</TableCell>
+                                                <TableCell className="text-center">{formatCurrency(item["Diferencia"])}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </Card>
+                        )}
+                    </TabsTrigger>
+
+                    {/* Tab para el id 2 */}
+                    <TabsTrigger value="tab2">
+                        {dataId2.length > 0 && (
+                            <Card className="mx-auto max-w-2xl mb-6">
+                                <p>
+                                    Cuadro 2:
+                                </p>
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableHeaderCell>INGRESOS CORRIENTES</TableHeaderCell>
+                                            <TableHeaderCell>UMBRAL MÁXIMO</TableHeaderCell>
+                                            <TableHeaderCell>AVANCE</TableHeaderCell>
+                                            <TableHeaderCell>DIFERENCIA</TableHeaderCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {dataId2.map((item) => (
+                                            <TableRow key={item.id}>
+                                                <TableCell className="text-center">{formatCurrency(item["Ingresos Corrientes"])}</TableCell>
+                                                <TableCell className="text-center">{formatCurrency(item["Umbral máximo"])}</TableCell>
+                                                <TableCell className="text-center">{item["Avance"].toFixed(2)}%</TableCell>
+                                                <TableCell className="text-center">{formatCurrency(item["Diferencia"])}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </Card>
+                        )}
+                    </TabsTrigger>
+                </TabsList>
+
+                <div className="ml-2 mt-4">
+                    {/* Content for tab1 */}
+                    <TabsContent value="tab1">
+                        <div className="mx-auto max-w-4xl">
+                            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+                                UMBRAL MINIMO
+                            </p>
+                            <BarChart
+                                data={dataId1}
+                                index="id"
+                                categories={["Umbral mínimo", "Ingresos Corrientes"]} // Nombres amigables en las categorías
+                                valueFormatter={dataFormatter}
+                                yAxisWidth={88}
+                                colors={["blue", "amber"]} 
+                                className="h-2xl" // Controla la altura del gráfico
+                            />
+                        </div>
+                    </TabsContent>
+
+                    {/* Content for tab2 */}
+                    <TabsContent value="tab2">
+                        <div className="mx-auto max-w-4xl"> {/* Ajusta el ancho del contenedor */}
+                            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+                                UMBRAL MAXIMO
+                            </p>
+                            <BarChart
+                                data={dataId2}
+                                index="id"
+                                categories={["Umbral máximo", "Ingresos Corrientes"]}
+                                valueFormatter={dataFormatter}
+                                yAxisWidth={88}
+                                colors={["blue", "amber"]} // Cambia los colores aquí
+                                className="h-2xl"
+                            />
+
+                        </div>
+                    </TabsContent>
+                </div>
+            </Tabs>
+        </>
+    );
+};
