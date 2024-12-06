61df729ad70b15698401c6e6337495d631d006a1
Pue223 <mdplmdpl5@gmail.com>
2024-12-06 16:01:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import {  Meta} from "./Meta.jsx";
 export const App = () => {
   return (
     <div>
-      <Card>
+          <Card>
+             <img
+          src="https://portal.muniplibre.gob.pe/wp-content/uploads/2019/03/logo-mdpl-1.png"
+          alt="Logo Municipalidad"
+          className="mx-auto mb-4 w-90" // Agrega clases de estilo
+        />
         <Meta/>
       </Card>
     </div>
