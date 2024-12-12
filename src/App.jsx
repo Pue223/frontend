b4113fda924fb5928a592b3bbe6f2936a92fbc2d
Pue223 <mdplmdpl5@gmail.com>
2024-12-12 11:46:17 -0500

    listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import {  Card} from '@tremor/react';
 import {  Meta} from "./Meta.jsx";
+import {  Ingresos} from "./Ingresos.jsx";
 export const App = () => {
   return (
-    <div>
-          <Card>
-             <img
-          src="https://portal.muniplibre.gob.pe/wp-content/uploads/2019/03/logo-mdpl-1.png"
-          alt="Logo Municipalidad"
-          className="mx-auto mb-4 w-90" // Agrega clases de estilo
-        />
-        <Meta/>
+    <div className="flex items-start">
+      <Card className="w-full flex items-start">
+        <div className="w-1/3"> {/* Contenedor de Ingresos */}
+        <img
+            src="https://portal.muniplibre.gob.pe/wp-content/uploads/2019/03/logo-mdpl-1.png"
+            alt="Logo Municipalidad"
+            className="mx-auto mb-4 w-90"
+          />
+          <Ingresos />
+        </div>
+        <div className="ml-1 flex flex-col justify-between w-2/3"> {/* Contenedor de Meta con menos margen */}
+          
+          <Meta />
+        </div>
       </Card>
     </div>
-  )
+  );
+  
+  
+  
 }
---
+++ src/Ingresos.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionList
+} from '@tremor/react';
+import { RiCalendar2Fill } from "@remixicon/react";
+import axios from 'axios';
+
+export const Ingresos = () => {
+  const [ingresos, setIngresos] = useState([]);
+
+  useEffect(() => {
+    // Realiza la solicitud al endpoint para obtener los ingresos
+    axios
+      .get('https://backendtw.onrender.com/api/ingresos')
+      .then((response) => {
+        if (response.data.ok) {
+          setIngresos(response.data.data);  // Guarda los ingresos en el estado
+        }
+      })
+      .catch((error) => {
+        console.error("Hubo un error al obtener los datos:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Accordion className="mt-3">
+        <AccordionList>
+          {/* Un solo AccordionHeader */}
+          <div>
+            <AccordionHeader>
+              <span className="flex items-center gap-2">
+                <RiCalendar2Fill className="size-4 text-blue-500" />
+                Ingresos
+              </span>
+            </AccordionHeader>
+            <AccordionBody>
+              <ol className="flex flex-col gap-2">
+                {ingresos.map((item, index) => (
+                  <li key={index}>
+                    <span className="font-semibold text-gray-900 dark:text-gray-50">
+                      Fecha:{item.fecha } - Ingreso:
+                    </span>{' '}
+                    {item.ingreso}
+                  </li>
+                ))}
+              </ol>
+            </AccordionBody>
+          </div>
+        </AccordionList>
+      </Accordion>
+    </>
+  );
+};
