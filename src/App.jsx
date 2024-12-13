923ae95a5c698591979cea37abdb294452afa496
Pue223 <mdplmdpl5@gmail.com>
2024-12-13 09:18:04 -0500

    editando_titulo

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import {  Meta} from "./Meta.jsx";
 import {  Ingresos} from "./Ingresos.jsx";
 export const App = () => {
   return (
-    <div className="flex items-start">
-      <Card className="w-full flex items-start">
-        <div className="w-1/3"> {/* Contenedor de Ingresos */}
-        <img
+    <> 
+     <img
             src="https://portal.muniplibre.gob.pe/wp-content/uploads/2019/03/logo-mdpl-1.png"
             alt="Logo Municipalidad"
             className="mx-auto mb-4 w-90"
           />
+    <div className="flex items-start">
+      <Card className="w-full flex items-start">
+        <div className="w-1/3"> {/* Contenedor de Ingresos */}       
           <Ingresos />
         </div>
         <div className="ml-1 flex flex-col justify-between w-2/3"> {/* Contenedor de Meta con menos margen */}
@@ -20,6 +21,7 @@ export const App = () => {
         </div>
       </Card>
     </div>
+    </>
   );
   
   
