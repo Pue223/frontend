dcbee258f40b8d5245e3b086404c2549fc982058
Pue223 <mdplmdpl5@gmail.com>
2024-12-19 12:59:43 -0500

    changeeee22

--- src/Meta.jsx
+++ src/Meta.jsx
@@ -172,7 +172,7 @@ export const Meta = () => {
                                                     <RiArrowUpDoubleFill color="green" />
                                                     {formatCurrency(item["Diferencia"])}
                                                 </TableCell>
-                                                   <TableCell className="text-center">
+                                                <TableCell className="text-center">
                                                     {item["Avance"].toFixed(2) > "100.00" ? (
                                                         <Badge className="bg-emerald-50 text-emerald-900 ring-emerald-600/30">
                                                             META CUMPLIDA
